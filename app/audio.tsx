f81e84ecf8f7edfa54c8208d9eb3fe01598efa00
Michelle <michelletan737@gmail.com>
2024-08-07 21:40:44 -0400

    project setup

---
+++ app/audio.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const audio = () => {
+  return (
+    <View>
+      <Text>audio</Text>
+    </View>
+  )
+}
+
+export default audio
+
+const styles = StyleSheet.create({})
---
+++ app/entry.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const entry = () => {
+  return (
+    <View>
+      <Text>entry</Text>
+    </View>
+  )
+}
+
+export default entry
+
+const styles = StyleSheet.create({})
